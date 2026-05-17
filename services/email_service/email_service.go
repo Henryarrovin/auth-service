92f5bdd05e30b8e9e69c440c9da869a97796b244
Henryarrovin <henry.a.s.arrovin@gmail.com>
2026-05-17 14:06:10 +0530

    added email_service

---
+++ services/email_service/email_service.go
@@ -0,0 +1,60 @@
+package email_service
+
+import (
+	"fmt"
+	"net/smtp"
+
+	"go.uber.org/zap"
+)
+
+type EmailConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	From     string
+	BaseURL  string
+}
+
+type EmailService struct {
+	cfg    EmailConfig
+	logger *zap.Logger
+}
+
+func NewEmailService(cfg EmailConfig, logger *zap.Logger) *EmailService {
+	return &EmailService{cfg: cfg, logger: logger}
+}
+
+func (s *EmailService) SendPasswordReset(email, token string) error {
+	resetLink := fmt.Sprintf("%s/reset-password?token=%s", s.cfg.BaseURL, token)
+
+	subject := "Password Reset Request"
+	body := fmt.Sprintf(`
+		Hello,
+
+		You requested a password reset. Click the link below to reset your password:
+
+		%s
+
+		This link expires in 15 minutes.
+
+		If you did not request this, ignore this email.
+		`, resetLink)
+
+	msg := fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s",
+		s.cfg.From, email, subject, body)
+
+	auth := smtp.PlainAuth("", s.cfg.Username, s.cfg.Password, s.cfg.Host)
+	addr := fmt.Sprintf("%s:%d", s.cfg.Host, s.cfg.Port)
+
+	if err := smtp.SendMail(addr, auth, s.cfg.From, []string{email}, []byte(msg)); err != nil {
+		s.logger.Error("failed to send reset email",
+			zap.String("email", email),
+			zap.Error(err),
+		)
+		return fmt.Errorf("sending reset email: %w", err)
+	}
+
+	s.logger.Info("reset email sent", zap.String("email", email))
+	return nil
+}
